6d6e9bb0e0335e9ea2ceeaf4f86e212d143c28f2
jblanco75 <jblanco75@gmail.com>
2021-12-01 10:36:56 -0500

    Ridin’ Bareback

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -4,7 +4,9 @@
 
 def mat_mul(mat1, mat2):
     """Performs matrix multiplication"""
-    if len(mat1[0]) == len(mat2):
+    if len(mat1[0]) != len(mat2):
+        return None
+    else:
         result = [[sum(i * j for i, j in zip(row, col))
                    for col in zip(*mat2)] for row in mat1]
     return result
