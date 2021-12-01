11200a8c66618a50b8ed8dd896f82e0c334eeabd
jblanco75 <jblanco75@gmail.com>
2021-12-01 10:31:06 -0500

    Gettin’ Cozy

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -4,11 +4,11 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """Concatenates depending axis value"""
-    if axis == 0:
+    if len(mat1[0]) == len(mat2[0]) and axis == 0:
         new = [i.copy() for i in mat1]
         new += [i.copy() for i in mat2]
         return new
-    elif axis == 1:
+    elif len(mat1) == len(mat2) and axis == 1:
         new = [mat1[i] + mat2[i] for i in range(len(mat1))]
         return new
     else:
