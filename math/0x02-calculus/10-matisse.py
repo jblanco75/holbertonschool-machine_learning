0c873a95b7c4396b78d38118353b39d052fdfd09
jblanco75 <jblanco75@gmail.com>
2021-12-08 20:30:06 -0500

    Derive happiness in oneself from a good day's work

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -5,10 +5,13 @@
 def poly_derivative(poly):
     """Returns a list with derivative of coef"""
     deriv = []
-    if poly is None:
+    if type(poly) is not list or len(poly) < 1:
         return None
     if len(poly) < 2:
         return [0]
+    for coef in poly:
+        if type(coef) is not int and type(coef) is not float:
+            return None
     for exp, coef in enumerate(poly):
         deriv.append(exp*coef)
     deriv.pop(0)
