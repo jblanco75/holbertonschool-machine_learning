025eb137239554f251f485cf16234758c6d31c80
jblanco75 <jblanco75@gmail.com>
2021-12-08 20:05:08 -0500

    Integrate

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -9,6 +9,11 @@ def poly_integral(poly, C=0):
         return None
     if type(C) is not int and type(C) is not float:
         return None
+    for coef in poly:
+        if type(coef) is not int and type(coef) is not float:
+            return None
+    if type(C) is float and C is int:
+        C = int(C)
     for exp, coef in enumerate(poly):
         integral.append((1/(exp+1))*coef)
     integral = [int(i) if i % 1 == 0 else i for i in integral]
