37711b696f1d44c38e5702444049634a7e3ccd26
jblanco75 <jblanco75@gmail.com>
2021-12-08 20:21:53 -0500

    Integrate

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,7 +4,6 @@
 
 def poly_integral(poly, C=0):
     """Returns integral of a polynomial"""
-    integral = [C]
     if type(poly) is not list or len(poly) < 1:
         return None
     if type(C) is not int and type(C) is not float:
@@ -14,6 +13,9 @@ def poly_integral(poly, C=0):
             return None
     if type(C) is float and C is int:
         C = int(C)
+    if poly == [0]:
+        return [C]
+    integral = [C]
     for exp, coef in enumerate(poly):
         integral.append((1/(exp+1))*coef)
     integral = [int(i) if i % 1 == 0 else i for i in integral]
