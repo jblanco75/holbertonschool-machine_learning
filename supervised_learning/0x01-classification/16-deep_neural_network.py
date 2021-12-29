5476ef183cab0d33cf5df3f8b60078dc0d80157a
jblanco75 <jblanco75@gmail.com>
2021-12-29 18:31:28 -0500

    DeepNeuralNetwork

---
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+Class that defines a deep neural network
+performing binary classification
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    Defines the DeepNeuralNetwork class
+    """
+    def __init__(self, nx, layers):
+        """Class constructor"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list or len(layers) < 1:
+            raise TypeError('layers must be a list of positive integers')
+        self.nx = nx
+        self.layers = layers
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+        for i in range(len(layers)):
+            if layers[i] < 1:
+                raise TypeError('layers must be a list of positive integers')
+            if i == 0:
+                self.weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], nx) * np.sqrt(2 / nx)
+            else:
+                self.weights['W' + str(i + 1)] = np.random.randn(
+                    layers[i], layers[i - 1]
+                ) * np.sqrt(2 / layers[i - 1])
+                self.weights['b' + str(i + 1)] = np.zeros(
+                    layers[i]
+                ).reshape(layers[i], 1)
