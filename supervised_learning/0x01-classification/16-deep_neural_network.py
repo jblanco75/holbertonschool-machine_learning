53e82fd4133576a431a80edd3522c206e86774e9
jblanco75 <jblanco75@gmail.com>
2021-12-29 18:41:30 -0500

    DeepNeuralNetwork

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -33,6 +33,4 @@ class DeepNeuralNetwork:
                 self.weights['W' + str(i + 1)] = np.random.randn(
                     layers[i], layers[i - 1]
                 ) * np.sqrt(2 / layers[i - 1])
-                self.weights['b' + str(i + 1)] = np.zeros(
-                    layers[i]
-                ).reshape(layers[i], 1)
+                self.weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
