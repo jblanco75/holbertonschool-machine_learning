161f456e6ba2ad33f9992dcc52fc9c64e8cda91f
jblanco75 <jblanco75@gmail.com>
2021-12-29 15:08:45 -0500

    NeuralNetwork Gradient Descent

---
+++ supervised_learning/0x01-classification/13-neural_network.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+"""
+Class that defines a neural network with
+ one hidden layer performing binary classification
+"""
+
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    Defines a neural network with one hidden
+    layer performing binary classification
+    """
+    def __init__(self, nx, nodes):
+        """
+        Class Constructor
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neural network
+        """
+        z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-z1))
+        z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-z2))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression
+        """
+        cost = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = np.sum(cost)
+        cost = - cost / A.shape[1]
+        return cost
+
+    def evaluate(self, X, Y):
+        """
+        Evaluates the neural network's predictions
+        """
+        self.forward_prop(X)
+        prediction = np.where(self.__A2 >= 0.5, 1, 0)
+        cost = self.cost(Y, self.__A2)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """
+        Calculates one pass of gradient descent on the neural network
+        """
+        # gradient descent for hidden layer
+        dz2 = A2 - Y
+        dw2 = np.matmul(A1, dz2.T) / A1.shape[1]
+        db2 = np.sum(dz2, axis=1, keepdims=True) / A2.shape[1]
+
+        # derivative of the sigmoid function
+        da1 = A1 * (1 - A1)
+        # gradient descent for output layer
+        dz1 = np.matmul(self.__W2.T, dz2)
+        dz1 = dz1 * da1
+        dw1 = np.matmul(X, dz1.T) / A1.shape[1]
+        db1 = np.sum(dz1, axis=1, keepdims=True) / A1.shape[1]
+        # updated value for weights and bias
+        self.__W2 = self.__W2 - alpha * dw2.T
+        self.__b2 = self.__b2 - alpha * db2
+        self.__W1 = self.__W1 - alpha * dw1.T
+        self.__b1 = self.__b1 - alpha * db1
+
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
