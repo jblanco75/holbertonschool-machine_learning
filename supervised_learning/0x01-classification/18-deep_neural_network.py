2c82a0af718e99be80f59cbc33272875f1fba272
jblanco75 <jblanco75@gmail.com>
2021-12-29 23:00:54 -0500

    DeepNeuralNetwork Forward Propagation

---
+++ supervised_learning/0x01-classification/18-deep_neural_network.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""
+Class that defines a deep neural network
+performing binary classification
+"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """
+    Defines the DeepNeuralNetwork class
+    """
+    def __init__(self, nx, layers):
+        """Class constructor"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list or len(layers) < 1:
+            raise TypeError('layers must be a list of positive integers')
+        inputs = nx
+        self.__L = len(layers)
+        self.__cache = {}
+        weights = {}
+        for index, layer in enumerate(layers, 1):
+            if type(layer) is not int or layer < 0:
+                raise TypeError("layers must be a list of positive integers")
+            weights["b{}".format(index)] = np.zeros((layer, 1))
+            weights["W{}".format(index)] = (
+                np.random.randn(layer, inputs) * np.sqrt(2 / inputs))
+            inputs = layer
+        self.__weights = weights
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neural network
+        """
+        self.__cache["A0"] = X
+        for index in range(self.L):
+            W = self.weights["W{}".format(index + 1)]
+            b = self.weights["b{}".format(index + 1)]
+            z = np.matmul(W, self.cache["A{}".format(index)]) + b
+            A = 1 / (1 + (np.exp(-z)))
+            self.__cache["A{}".format(index + 1)] = A
+        return (A, self.cache)
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights
