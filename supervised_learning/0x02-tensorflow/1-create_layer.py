1322691991c024615a3589011c57eb3147ace348
jblanco75 <jblanco75@gmail.com>
2022-01-06 16:39:02 -0500

    Layers

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -12,5 +12,5 @@ def create_layer(prev, n, activation):
     """Returns: the tensor output of the layer"""
     weights = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     layer = tf.layers.Dense(n, activation=activation,
-                                  kernel_initializer=weights, name='layer')
+                            kernel_initializer=weights, name='layer')
     return layer(prev)
