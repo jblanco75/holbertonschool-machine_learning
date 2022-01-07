b28fdb63e4b6013f711a1dc35393f81f41793f89
jblanco75 <jblanco75@gmail.com>
2022-01-06 22:37:12 -0500

    Train

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -18,18 +18,18 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
     """
     Returns: the path where the model was saved
     """
-    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
-    y_pred = forward_prop(x, layer_sizes, activations)
-    accuracy = calculate_accuracy(y, y_pred)
-    loss = calculate_loss(y, y_pred)
-    train_op = create_train_op(loss, alpha)
-
     tf.add_to_collection('x', x)
     tf.add_to_collection('y', y)
     tf.add_to_collection('y_pred', y_pred)
     tf.add_to_collection('loss', loss)
     tf.add_to_collection('accuracy', accuracy)
     tf.add_to_collection('train_op', train_op)
+
+    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    y_pred = forward_prop(x, layer_sizes, activations)
+    accuracy = calculate_accuracy(y, y_pred)
+    loss = calculate_loss(y, y_pred)
+    train_op = create_train_op(loss, alpha)
 
     init = tf.global_variables_initializer()
     sess = tf.Session()
