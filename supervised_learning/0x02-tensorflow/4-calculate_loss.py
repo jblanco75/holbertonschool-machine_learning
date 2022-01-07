ffb756b8eee0008b6a3c4ec714f2c8c241e55b67
jblanco75 <jblanco75@gmail.com>
2022-01-06 19:56:21 -0500

    Loss

--- supervised_learning/0x02-tensorflow/4-calculate_loss.py
+++ supervised_learning/0x02-tensorflow/4-calculate_loss.py
@@ -10,5 +10,5 @@ tf.disable_eager_execution()
 
 def calculate_loss(y, y_pred):
     """Returns: a tensor containing the loss of the prediction"""
-    loss = tf.keras.losses.CategoricalCrossentropy(y, y_pred)
+    loss = tf.losses.softmax_cross_entropy(y, y_pred)
     return loss
