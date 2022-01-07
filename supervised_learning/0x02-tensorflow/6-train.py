4cfc7e8042305dbde3f95d4296593bee8dfbc593
jblanco75 <jblanco75@gmail.com>
2022-01-06 22:25:01 -0500

    Train

---
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+"""
+Function that builds, trains, and saves a neural network classifier
+"""
+
+
+import tensorflow.compat.v1 as tf
+tf.disable_eager_execution()
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+create_train_op = __import__('5-create_train_op').create_train_op
+forward_prop = __import__('2-forward_prop').forward_prop
+
+
+def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
+          alpha, iterations, save_path="/tmp/model.ckpt"):
+    """
+    Returns: the path where the model was saved
+    """
+    tf.add_to_collection('x', x)
+    tf.add_to_collection('y', y)
+    tf.add_to_collection('y_pred', y_pred)
+    tf.add_to_collection('loss', loss)
+    tf.add_to_collection('accuracy', accuracy)
+    tf.add_to_collection('train_op', train_op)
+
+    x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
+    y_pred = forward_prop(x, layer_sizes, activations)
+    accuracy = calculate_accuracy(y, y_pred)
+    loss = calculate_loss(y, y_pred)
+    train_op = create_train_op(loss, alpha)
+
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+
+    for i in range(iterations):
+        train_loss = sess.run(loss, feed_dict={x: X_train, y: Y_train})
+        train_acc = sess.run(accuracy, feed_dict={x: X_train, y: Y_train})
+        valid_loss = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
+        valid_acc = sess.run(accuracy, feed_dict={x: X_valid, y: Y_valid})
+        if i % 100 == 0 or i == 0 or i == iterations:
+            print("After {} iterations:".format(i))
+            print("\tTraining Cost: {}".format(train_loss))
+            print("\tTraining Accuracy: {}".format(train_acc))
+            print("\tValidation Cost: {}".format(valid_loss))
+            print("\tValidation Accuracy: {}".format(valid_acc))
+        if i < iterations:
+            sess.run(train_op, feed_dict={x: X_train, y: Y_train})
+        save_path = saver.save(sess, save_path)
+    return save_path
