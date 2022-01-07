3c19987d23505ee69912335f8ea30b6c2760aed6
jblanco75 <jblanco75@gmail.com>
2022-01-07 00:12:51 -0500

    Evaluate

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -25,4 +25,5 @@ def evaluate(X, Y, save_path):
         prediction = sess.run(y_pred, feed_dict={x: X, y: Y})
         accuracy = sess.run(accuracy, feed_dict={x: X, y: Y})
         loss = sess.run(loss, feed_dict={x: X, y: Y})
-        return prediction, accuracy, loss
+
+        return prediction[0], accuracy[0], loss[0]
