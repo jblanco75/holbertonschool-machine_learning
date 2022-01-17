9b6299d9b6e9df03dc112af548308f5327138f4d
jblanco75 <jblanco75@gmail.com>
2022-01-17 10:47:37 -0500

    Mini Batch

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -31,38 +31,38 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
         else:
             n_batches = m // batch_size + 1
 
-            for epoch in range(epochs + 1):
-                cost_train = sess.run(loss, feed_dict={x: X_train, y: Y_train})
-                accuracy_train = sess.run(accuracy,
-                                          feed_dict={x: X_train, y: Y_train})
-                cost_val = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
-                accuracy_val = sess.run(accuracy,
-                                        feed_dict={x: X_valid, y: Y_valid})
-                print("After {} epochs:".format(epoch))
-                print("\tTraining Cost: {}".format(cost_train))
-                print("\tTraining Accuracy: {}".format(accuracy_train))
-                print("\tValidation Cost: {}".format(cost_val))
-                print("\tValidation Accuracy: {}".format(accuracy_val))
+        for epoch in range(epochs + 1):
+            cost_train = sess.run(loss, feed_dict={x: X_train, y: Y_train})
+            accuracy_train = sess.run(accuracy,
+                                      feed_dict={x: X_train, y: Y_train})
+            cost_val = sess.run(loss, feed_dict={x: X_valid, y: Y_valid})
+            accuracy_val = sess.run(accuracy,
+                                    feed_dict={x: X_valid, y: Y_valid})
+            print("After {} epochs:".format(epoch))
+            print("\tTraining Cost: {}".format(cost_train))
+            print("\tTraining Accuracy: {}".format(accuracy_train))
+            print("\tValidation Cost: {}".format(cost_val))
+            print("\tValidation Accuracy: {}".format(accuracy_val))
 
-                if epoch < epochs:
-                    shuffled_X, shuffled_Y = shuffle_data(X_train, Y_train)
-                    for batch in range(n_batches):
-                        start = batch * batch_size
-                        end = (batch + 1) * batch_size
-                        if end > m:
-                            end = m
-                        X_mini_batch = shuffled_X[start:end]
-                        Y_mini_batch = shuffled_Y[start:end]
-                        next_train = {x: X_mini_batch, y: Y_mini_batch}
-                        sess.run(train_op, feed_dict=next_train)
+            if epoch < epochs:
+                shuffled_X, shuffled_Y = shuffle_data(X_train, Y_train)
+                for batch in range(n_batches):
+                    start = batch * batch_size
+                    end = (batch + 1) * batch_size
+                    if end > m:
+                        end = m
+                    X_mini_batch = shuffled_X[start:end]
+                    Y_mini_batch = shuffled_Y[start:end]
+                    next_train = {x: X_mini_batch, y: Y_mini_batch}
+                    sess.run(train_op, feed_dict=next_train)
 
-                        if (batch + 1) % 100 == 0 and batch != 0:
-                            loss_mini_batch = sess.run(loss,
-                                                       feed_dict=next_train)
-                            acc_mini_batch = sess.run(accuracy,
-                                                      feed_dict=next_train)
-                            print("\tStep {}:".format(batch + 1))
-                            print("\t\tCost: {}".format(loss_mini_batch))
-                            print("\t\tAccuracy: {}".format(acc_mini_batch))
+                    if (batch + 1) % 100 == 0 and batch != 0:
+                        loss_mini_batch = sess.run(loss,
+                                                   feed_dict=next_train)
+                        acc_mini_batch = sess.run(accuracy,
+                                                  feed_dict=next_train)
+                        print("\tStep {}:".format(batch + 1))
+                        print("\t\tCost: {}".format(loss_mini_batch))
+                        print("\t\tAccuracy: {}".format(acc_mini_batch))
 
-            return saver.save(sess, save_path)
+        return saver.save(sess, save_path)
