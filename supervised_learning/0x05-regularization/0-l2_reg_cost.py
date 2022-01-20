d570461c937d2fe0b552ed73a149eb44e8f31fb0
jblanco75 <jblanco75@gmail.com>
2022-01-19 22:27:59 -0500

    L2 Regularization Cost

--- supervised_learning/0x05-regularization/0-l2_reg_cost.py
+++ supervised_learning/0x05-regularization/0-l2_reg_cost.py
@@ -13,9 +13,9 @@ def l2_reg_cost(cost, lambtha, weights, L, m):
     Returns: the cost of the network
     accounting for L2 regularization
     """
-    L2 = 0
-    for i in range(1, L + 1):
-        w = weights['W{}'.format(i)]
-        W_norm = np.linalg.norm(w)
+    W_norm = 0
+    for i in range(L):
+        w = weights['W{}'.format(i + 1)]
+        W_norm += np.linalg.norm(w)
         L2 = (lambtha / (2 * m)) * W_norm
     return cost + L2
