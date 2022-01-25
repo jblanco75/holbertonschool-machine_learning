d335f89c6b2b4caac48f599af3c5bbac5cdbe363
jblanco75 <jblanco75@gmail.com>
2022-01-25 15:09:04 -0500

    Early Stopping

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Function that trains a model using mini-batch gradient descent
+"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False,
+                patience=0, verbose=True, shuffle=False):
+    if early_stopping and validation_data:
+        callback = K.callbacks.EarlyStopping(monitor='val_loss',
+                                             patience=patience,
+                                             mode='min')
+        return network.fit(x=data, y=labels,
+                           batch_size=batch_size,
+                           epochs=epochs,
+                           validation_data=validation_data,
+                           verbose=verbose,
+                           callbacks=callback,
+                           shuffle=shuffle)
