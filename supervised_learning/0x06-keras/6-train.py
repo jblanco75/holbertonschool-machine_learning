56f79dd68183c49956cb22e0548c877568775d17
jblanco75 <jblanco75@gmail.com>
2022-01-25 15:55:05 -0500

    Early Stopping

--- supervised_learning/0x06-keras/6-train.py
+++ supervised_learning/0x06-keras/6-train.py
@@ -10,6 +10,9 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False,
                 patience=0, verbose=True, shuffle=False):
+    """
+    Trains the model using early stopping
+    """
     if early_stopping and validation_data:
         callback = K.callbacks.EarlyStopping(monitor='val_loss',
                                              patience=patience,
