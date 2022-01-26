9c4d044a0ef7e8afb7bbbdf44b1ce34698858f86
jblanco75 <jblanco75@gmail.com>
2022-01-25 21:04:18 -0500

    Test

---
+++ supervised_learning/0x06-keras/12-test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Function that tests a neural network
+"""
+
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """
+    Returns: the loss and accuracy of the model
+    with the testing data, respectively
+    """
+    loss, accuracy = network.evaluate(x=data,
+                                      y=labels,
+                                      verbose=verbose)
+    return loss, accuracy
