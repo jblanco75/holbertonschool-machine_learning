886358a534dec47bb3633ad113e05960f8f39185
jblanco75 <jblanco75@gmail.com>
2022-01-25 21:17:25 -0500

    Predict

---
+++ supervised_learning/0x06-keras/13-predict.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Function that makes a prediction using a neural network
+"""
+
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """
+    Returns: the prediction for the data
+    """
+    return network.prediction(x=data, verbose=verbose)
