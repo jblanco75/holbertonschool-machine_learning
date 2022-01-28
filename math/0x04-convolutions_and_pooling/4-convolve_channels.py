06d41d6572e8078c640e2e730453adaae2d3b180
jblanco75 <jblanco75@gmail.com>
2022-01-27 22:52:24 -0500

    Convolution with Channels

--- math/0x04-convolutions_and_pooling/4-convolve_channels.py
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -41,7 +41,7 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     if padding == 'same':
         ph = ((((h - 1) * sh) + kh - h) // 2) + 1
         pw = ((((w - 1) * sw) + kw - w) // 2) + 1
-    elif padding is 'valid':
+    elif padding == 'valid':
         ph = 0
         pw = 0
     else:
@@ -49,7 +49,7 @@ def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
     new_padded_images = np.pad(images, ((0, 0),
                                         (ph, ph),
                                         (pw, pw),
-                                        (0, 0)))
+                                        (0, 0)), 'constant')
     o_h = ((h + (2 * ph) - kh) // sh) + 1
     o_w = ((w + (2 * pw) - kw) // sw) + 1
     output = np.zeros((m, o_h, o_w))
