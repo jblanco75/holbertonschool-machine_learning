8f68739cc2063494a9dcc6fd43432080660f8f91
jblanco75 <jblanco75@gmail.com>
2022-02-09 18:56:54 -0500

    Inception Network

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -6,7 +6,7 @@ described in Going Deeper with Convolutions (2014)
 
 
 import tensorflow.keras as K
-inception_network = __import__('1-inception_network').inception_network
+inception_block = __import__('0-inception_block').inception_block
 
 
 def inception_network():
