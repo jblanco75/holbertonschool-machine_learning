f50c262dfd0d5e5e9f2a53424352b22c1d1725fb
jblanco75 <jblanco75@gmail.com>
2022-02-09 19:37:48 -0500

    Inception Network

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -23,8 +23,8 @@ def inception_network():
                              padding='same',
                              activation='relu',
                              strides=2,
-                             kernel_initializer=w)(input)
-    pool_1 = K.layers.MaxPooling2D(pool_size=3, strides=2,
+                             kernel_initializer=w)(inputs)
+    pool_1 = K.layers.MaxPool2D(pool_size=3, strides=2,
                                    padding='same')(conv_1)
     conv_2R = K.layers.Conv2D(filters=64,
                               kernel_size=(1, 1),
@@ -38,18 +38,18 @@ def inception_network():
                              activation='relu',
                              strides=1,
                              kernel_initializer=w)(conv_2R)
-    pool_2 = K.layers.MaxPooling2D(pool_size=3, strides=2,
+    pool_2 = K.layers.MaxPool2D(pool_size=3, strides=2,
                                    padding='same')(conv_2)
     inblock_3a = inception_block(pool_2, [64, 96, 128, 16, 32, 32])
     inblock_3b = inception_block(inblock_3a, [128, 128, 192, 32, 96, 64])
-    pool_3 = K.layers.MaxPooling2D(pool_size=3, strides=2,
+    pool_3 = K.layers.MaxPool2D(pool_size=3, strides=2,
                                    padding='same')(inblock_3b)
     inblock_4a = inception_block(pool_3, [192, 96, 208, 16, 48, 64])
     inblock_4b = inception_block(inblock_4a, [160, 112, 224, 24, 64, 64])
     inblock_4c = inception_block(inblock_4b, [128, 128, 256, 24, 64, 64])
     inblock_4d = inception_block(inblock_4c, [112, 144, 288, 32, 64, 64])
     inblock_4e = inception_block(inblock_4d, [256, 160, 320, 32, 128, 128])
-    pool_4 = K.layers.MaxPooling2D(pool_size=3, strides=2,
+    pool_4 = K.layers.MaxPool2D(pool_size=3, strides=2,
                                    padding='same')(inblock_4e)
     inblock_5a = inception_block(pool_4, [256, 160, 320, 32, 128, 128])
     inblock_5b = inception_block(inblock_5a, [384, 192, 384, 48, 128, 128])
