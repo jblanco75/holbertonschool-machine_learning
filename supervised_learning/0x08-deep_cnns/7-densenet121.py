b344fc062e00b15076e2c77a58d97bc31e03d4b4
jblanco75 <jblanco75@gmail.com>
2022-02-11 10:00:48 -0500

    Fix

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -3,8 +3,6 @@
 Function that builds the DenseNet-121 architecture as
 described in Densely Connected Convolutional Networks
 """
-
-
 import tensorflow.keras as K
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
