7c085285a5b707f27d13ae045d7f1c50562bff44
jblanco75 <jblanco75@gmail.com>
2022-02-11 10:24:50 -0500

    Fix

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -11,15 +11,6 @@ transition_layer = __import__('6-transition_layer').transition_layer
 def densenet121(growth_rate=32, compression=1.0):
     """
     growth_rate is the growth rate
-    compression is the compression factor
-    You can assume the input data will have shape (224, 224, 3)
-    All convolutions should be preceded by Batch Normalization and a
-    rectified linear activation (ReLU), respectively
-    All weights should use he normal initialization
-    You may use:
-    dense_block = __import__('5-dense_block').dense_block
-    transition_layer = __import__('6-transition_layer').transition_layer
-    Returns: the keras model
     """
     w = K.initializers.HeNormal()
     input_layer = K.Input(shape=(224, 224, 3))
