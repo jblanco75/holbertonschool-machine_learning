648597073daa857f119c5c2375f9777f9cd92a9e
jblanco75 <jblanco75@gmail.com>
2022-03-17 15:49:22 -0500

    Determinant

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -20,34 +20,35 @@ def determinant(matrix):
     if matrix == [[]]:
         return 1
 
-    for i in range(len(matrix)):
-        if len(matrix) != len(matrix[i]):
-            raise ValueError("matrix must be a square matrix")
-        if type(matrix[i]) is not list or not len(matrix[i]):
+    for row in matrix:
+        if type(row) is not list:
             raise TypeError("matrix must be a list of lists")
-
-        if len(matrix) == 1:
+        if len(row) is 0 and len(matrix) is 1:
+            return 1
+        if len(row) != len(matrix):
+            raise ValueError("matrix must be a square matrix")
+    if len(matrix) == 1:
             return matrix[0][0]
 
-        if len(matrix) == 2:
-            return (matrix[0][0] *
-                    matrix[1][1]) - (matrix[0][1] *
-                                     matrix[1][0])
+    if len(matrix) == 2:
+        return (matrix[0][0] *
+                matrix[1][1]) - (matrix[0][1] *
+                                 matrix[1][0])
 
-        cof = 1
-        d = 0
-        for i in range(len(matrix)):
-            element = matrix[0][i]
-            sub_matrix = []
-            for row in range(len(matrix)):
-                if row == 0:
+    cof = 1
+    d = 0
+    for i in range(len(matrix)):
+        element = matrix[0][i]
+        sub_matrix = []
+        for row in range(len(matrix)):
+            if row == 0:
+                continue
+            new_row = []
+            for column in range(len(matrix)):
+                if column == i:
                     continue
-                new_row = []
-                for column in range(len(matrix)):
-                    if column == i:
-                        continue
-                    new_row.append(matrix[row][column])
-                sub_matrix.append(new_row)
-            d += (element * cof * determinant(sub_matrix))
-            cof *= -1
-        return (d)
+                new_row.append(matrix[row][column])
+            sub_matrix.append(new_row)
+        d += (element * cof * determinant(sub_matrix))
+        cof *= -1
+    return (d)
