5809ce5e34c10984aad7a0256505a601c94507fb
jblanco75 <jblanco75@gmail.com>
2022-03-21 19:57:02 -0500

    Correlation

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -19,7 +19,7 @@ def correlation(C):
     """
     if not type(C) is np.ndarray:
         raise TypeError('C must be a numpy.ndarray')
-    elif len(C.shape) != 2:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError('C must be a 2D square matrix')
 
     var = np.sqrt(np.diag(C))
