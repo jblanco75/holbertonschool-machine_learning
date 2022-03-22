f84f4ea47c624a3c24b04a27fceda67d79d43d14
jblanco75 <jblanco75@gmail.com>
2022-03-21 19:54:19 -0500

    Correlation

---
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Function that calculates a correlation matrix
+"""
+
+
+import numpy as np
+
+
+def correlation(C):
+    """
+    C is a numpy.ndarray of shape (d, d) containing a covariance matrix
+      d is the number of dimensions
+      If C is not a numpy.ndarray, raise a TypeError with the message
+      C must be a numpy.ndarray
+      If C does not have shape (d, d), raise a ValueError with the
+      message C must be a 2D square matrix
+    Returns a numpy.ndarray of shape (d, d) containing the correlation matrix
+    """
+    if not type(C) is np.ndarray:
+        raise TypeError('C must be a numpy.ndarray')
+    elif len(C.shape) != 2:
+        raise ValueError('C must be a 2D square matrix')
+
+    var = np.sqrt(np.diag(C))
+    corr = C / np.outer(var, var)
+
+    return corr
