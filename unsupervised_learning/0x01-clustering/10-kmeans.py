5b00dcde79bb88dba4b92df989b6edfac103207a
jblanco75 <jblanco75@gmail.com>
2022-03-31 18:37:42 -0500

    Hello, sklearn!

--- unsupervised_learning/0x01-clustering/10-kmeans.py
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -1,33 +1,24 @@
 #!/usr/bin/env python3
 """
-Function that calculates a GMM from a dataset
+Function that performs K-means on a dataset
 """
 
 
-import sklearn.mixture
+import sklearn.cluster
 
 
-def gmm(X, k):
+def kmeans(X, k):
     """
     X is a numpy.ndarray of shape (n, d) containing the dataset
     k is the number of clusters
-    The only import you are allowed to use is import sklearn.mixture
-    Returns: pi, m, S, clss, bic
-      pi is a numpy.ndarray of shape (k,) containing the cluster priors
-      m is a numpy.ndarray of shape (k, d) containing the centroid means
-      S is a numpy.ndarray of shape (k, d, d) containing the covariance
-      matrices
-      clss is a numpy.ndarray of shape (n,) containing the cluster indices
-      for each data point
-      bic is a numpy.ndarray of shape (kmax - kmin + 1) containing the BIC
-      value for each cluster size tested
+    The only import you are allowed to use is import sklearn.cluster
+    Returns: C, clss
+      C is a numpy.ndarray of shape (k, d) containing the centroid means
+      for each cluster
+      clss is a numpy.ndarray of shape (n,) containing the index of the
+      cluster in C that each data point belongs to
     """
-    gm = sklearn.mixture.GaussianMixture(n_components=k).fit(X)
-
-    pi = gm.weights_
-    m = gm.means_
-    S = gm.covariances_
-    clss = gm.predict(X)
-    bic = gm.bic(X)
-
-    return pi, m, S, clss, bic
+    km = sklearn.cluster.KMeans(n_clusters=k).fit(X)
+    C = km.cluster_centers_
+    clss = km.labels_
+    return C, clss
