3c9ffc0dae29a864b9b1d47d13a6e56ae20f5ad3
jblanco75 <jblanco75@gmail.com>
2022-04-28 15:38:25 -0500

    Deep RNN

--- supervised_learning/0x0D-RNNs/4-deep_rnn.py
+++ supervised_learning/0x0D-RNNs/4-deep_rnn.py
@@ -16,7 +16,8 @@ def deep_rnn(rnn_cells, X, h_0):
       t is the maximum number of time steps
       m is the batch size
       i is the dimensionality of the data
-    h_0 is the initial hidden state, given as a numpy.ndarray of shape (l, m, h)
+    h_0 is the initial hidden state, given as a numpy.ndarray of
+    shape (l, m, h)
       h is the dimensionality of the hidden state
     Returns: H, Y
       H is a numpy.ndarray containing all of the hidden states
