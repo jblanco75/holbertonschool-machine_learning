2ea398b35f4ec074f35af92294f81394fbe9ac45
jblanco75 <jblanco75@gmail.com>
2022-05-22 20:21:14 -0500

    Transformer Network

--- supervised_learning/0x11-attention/11-transformer.py
+++ supervised_learning/0x11-attention/11-transformer.py
@@ -10,37 +10,20 @@ Decoder = __import__('10-transformer_decoder').Decoder
 
 
 class Transformer(tf.keras.Model):
-    """
-    Class to create an encoder for a transformer
-    """
-
-    def __init__(self, N, dm, h, hidden, input_vocab, target_vocab,
-                 max_seq_input, max_seq_target, drop_rate=0.1):
-        """
-        Class constructor
-        """
+    """ class to create a transformer """
+    def __init__(self, N, dm, h, hidden, input_vocab,
+                 target_vocab, max_seq_input, max_seq_target, drop_rate=0.1):
         super(Transformer, self).__init__()
         self.encoder = Encoder(N, dm, h, hidden, input_vocab,
                                max_seq_input, drop_rate)
         self.decoder = Decoder(N, dm, h, hidden, target_vocab,
                                max_seq_target, drop_rate)
-        self.linear = tf.keras.layers.Dense(target_vocab)
+        self.final_layer = tf.keras.layers.Dense(target_vocab)
 
     def call(self, inputs, target, training, encoder_mask, look_ahead_mask,
              decoder_mask):
-        """
-        inputs - a tensor of shape (batch, input_seq_len)containing the inputs
-        target - a tensor of shape (batch, target_seq_len)containing the target
-        training - a boolean to determine if the model is training
-        encoder_mask - the padding mask to be applied to the encoder
-        look_ahead_mask - the look ahead mask to be applied to the decoder
-        decoder_mask - the padding mask to be applied to the decoder
-        Returns: a tensor of shape (batch, target_seq_len, target_vocab)
-        containing the transformer output
-        """
         encoder_output = self.encoder(inputs, training, encoder_mask)
         decoder_output = self.decoder(target, encoder_output, training,
                                       look_ahead_mask, decoder_mask)
-        output = self.linear(decoder_output)
-
+        output = self.final_layer(decoder_output)
         return output
